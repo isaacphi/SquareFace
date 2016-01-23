efae23d9c0e3339573260a5e1e568a88da85c490
Phil Isaac <isaac.phil@gmail.com>
2016-01-23 18:50:13 -0500

    updated grayscale squareface

---
+++ Assets/rotate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody body;
+
+	// Use this for initialization
+	void Awake () {
+		body = GetComponent<Rigidbody> ();
+		body.angularVelocity = Vector3.up * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
